a314f95ed170d3adc17034f508776e8278a3d1d7
Seyeyi <selamawitzemecha@gmail.com>
2022-03-15 17:37:42 +0300

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-
-void _putchar.c;
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,5 +12,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-char *rot13(char *);
-
+#endif
