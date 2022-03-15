0e3b957cc9fa0c866c4ff989ed2943ea3e2de32e
Seyeyi <selamawitzemecha@gmail.com>
2022-03-15 21:27:43 +0300

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the first 100 Fibonacci numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	double a = 0;
+	double b = 1;
+	double f;
+	int i;
+
+	for (i = 0; i < 50; i++)
+	{
+		f = a + b;
+		if (i != 49)
+			printf("%.f, ", f);
+		else
+			printf("%.f\n", f);
+		a = b;
+		b = f;
+	}
+	return (0);
+}
+
