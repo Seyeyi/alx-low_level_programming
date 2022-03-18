accc72e1fb6adf9f1b04ece0feb64efa57e517d1
Seyeyi <selamawitzemecha@gmail.com>
2022-03-18 09:06:10 +0300

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers
- * @c: param
+ * print_numbers - checks the code
  * Return: Always 0
  */
-int print_numbers(int c)
+int print_numbers(void)
 {
+	int c;
+
 	for (c >= 48; c <= 57; c++)
+	{
 		_putchar(c);
+	}
 	_putchar("\n");
 }
