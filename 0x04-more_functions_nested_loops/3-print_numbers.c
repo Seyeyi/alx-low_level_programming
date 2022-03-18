bbc3af606ac50c419d138dacc87465b1d65d68d6
Seyeyi <selamawitzemecha@gmail.com>
2022-03-18 22:59:09 +0300

    numbs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,13 @@
 #include "main.h"
-/**
- * print_numbers - checks the code
- *
- * Return: Always 0
- */
-int print_numbers(void)
-{
-	int c;
 
-	for (c >= 48; c <= 57; c++)
-	{
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 		_putchar(c);
-	}
 	_putchar('\n');
-	return (0);
 }
