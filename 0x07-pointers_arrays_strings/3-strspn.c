eb3a797cfe14568eac10d654619681cb9331d774
Seyeyi <selamawitzemecha@gmail.com>
2022-03-29 04:35:09 +0300

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * * _strspn - gets the length of a prefix substring
- * * @s: string
- * * @accept: contains bytes that may or may not compose parts of the string
- * *
- * * Return: the number of bytes that compose the length
- * */
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: contains bytes that may or may not compose parts of the string
+ *
+ * Return: the number of bytes that compose the length
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
