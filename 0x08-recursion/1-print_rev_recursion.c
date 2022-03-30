dd6619eb2b43fb6cfdf63cd5beb806d0b88c561c
Seyeyi <selamawitzemecha@gmail.com>
2022-03-30 23:54:29 +0300

    rec

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,12 +2,12 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse.
- * @s: string
+ * @s: pointer to the string
  * Return: Always 0.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+if (*s == '\0')
 	{
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
